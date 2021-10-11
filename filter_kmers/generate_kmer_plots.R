bd337a877c239d0a55c1d53b341a34206a1de773
dcsoto <dcsoto@ucdavis.edu>
2021-10-11 15:46:45 -0700

    Add code for plotting kmer distibutions

---
+++ filter_kmers/generate_kmer_plots.R
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+
+library("ggplot2")
+
+args <- commandArgs(TRUE) 
+filename <- args[1]
+
+# Read input file
+data <- read.table(filename, sep="\t", col.names = c("kmers", "counts"))
+
+# Plots
+ggplot(data, aes(x=counts)) + geom_histogram() + theme_bw()
+ggsave(paste(filename,"_hist.png", sep=""))
+
+ggplot(data, aes(x=counts)) + geom_boxplot() + theme_bw()
+ggsave(paste(filename,"_boxplot.png", sep=""))
+
+ggplot(data, aes(x=counts)) + stat_ecdf(geom = "point") + theme_bw()
+ggsave(paste(filename,"_ecdf.png", sep="")) 
