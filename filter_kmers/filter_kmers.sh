c4670a53f900e27519f56b5e681cd7b954e7e673
dcsoto <dcsoto@ucdavis.edu>
2021-10-12 17:27:23 -0700

    modify filter kmer

--- filter_kmers/filter_kmers.sh
+++ filter_kmers/filter_kmers.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-minkmercov=$1
+normalization_factor=$1
 input=$2
 output=$3
 
 if [ "$#" -ne 3 ]; then
-  echo "  Usage: bash filter_kmers.sh min_kmer_cov input output " >&2 ;
+  echo "  Usage: bash filter_kmers.sh normalization_factor input output " >&2 ;
   echo "Exiting now!!!"
   exit 1 ;
 fi
+
+minkmercov = 9/$normalization_factor
+echo " Min k-mer coverage is " $minkmercov 
 
 echo "  Removing likely erroneous k-mers "
 awk -F "\t" -v var=$minkmercov '{if($2>var){print}}' $input > $output
