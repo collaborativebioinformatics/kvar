43991b6a5e827c6a9735fb336b32cdf9e4786bbe
Dreycey <albindreycey@gmail.com>
2021-10-13 21:36:26 -0600

    modified functional script to fit into pipeline

--- functional_impact/get_functional_impact.sh
+++ functional_impact/get_functional_impact.sh
@@ -7,7 +7,7 @@ kmers_seq=$2
 gff_file=$3
 edit_distance=$4
 threads=$5
-
+output_directory=$6
 
 if [ "$#" -ne 3 ]; then
   echo "Usage: bash get_functional_impact.sh reference.fa kmers.fa annotation.gff edit_distance threads " >&2 ;
@@ -19,14 +19,11 @@ fi
 # Indexing human reference
 mrsfast --index ${reference} -e ${edit_distance}
 
-
 # Mapping k-mers
 mrsfast --search ${reference} --seq ${kmers_seq} --threads ${threads} -o mappings.sam
 
-
 # Calling variants
-freebayes -f $reference --min-alternate-count 1 mappings.sam > output.vcf
-
+freebayes -f ${reference} --min-alternate-count 1 mappings.sam > output.vcf
 
 # Functional impact wit VEP
 # GFF file must have index .tbi
