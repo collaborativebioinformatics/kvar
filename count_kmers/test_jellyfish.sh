27a950dfbdeed9a8d6ef1eb21704b95137fbe1de
Dreycey <albindreycey@gmail.com>
2021-10-14 00:36:52 -0600

    updating underlysing bach script

--- count_kmers/test_jellyfish.sh
+++ count_kmers/test_jellyfish.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-
 set -e
 
 #Assuming kmer size, genomic coverage, zipped paired end reads and output directory as inputs
@@ -10,6 +9,12 @@ file1=$3
 file2=$4
 outdir1=$5
 
+echo "kmer size: $kmersize" 
+echo "nuc coverage: $nuc_cov"
+echo "file 1: $file1"
+echo "file 2: $file2"
+echo "output directory: $outdir1"
+
 whole_number='^[0-9]+$'
 
 if [ "$#" -ne 5 ] || ! [[ ${nuc_cov} =~ ${whole_number} ]] || ! [[ ${kmersize} =~ ${whole_number} ]]; then
@@ -19,14 +24,12 @@ if [ "$#" -ne 5 ] || ! [[ ${nuc_cov} =~ ${whole_number} ]] || ! [[ ${kmersize} =
 fi
 
 echo "  Running Jellyfish counting "
-jellyfish count -C -m $kmersize -s 100M -t $(eval nproc) <(zcat $file1) <(zcat $file2) -o ${outdir1}_mer_counts.jf
+jellyfish count -C -m $kmersize -s 100M -t $(eval nproc) <(gzcat $file1) <(gzcat $file2) -o ${outdir1}_mer_counts.jf
 
 echo "  Creating histogram "
 jellyfish histo -t 10 ${outdir1}_mer_counts.jf > ${outdir1}_mer_counts.histo
 
 echo "  Dumping kmers "
 jellyfish dump ${outdir1}_mer_counts.jf > ${outdir1}_mer_counts.fasta
-cat ${outdir1}_mer_counts.fasta | paste - - | sed -e 's/^>//' | awk -F'\t' '{print $2"\t"$1}' | sort -k2,2nr > ${outdir1}_mer_counts.normalised.tsv
-# This also works ==> cat mer_counts.fasta | tr "\n" "\t" | sed -e 's/\t>/\n/g' -e 's/^>//' | awk -F'\t' '{print $2"\t"$1}' >mer_counts.normalised.tsv
-
-
+cat ${outdir1}_mer_counts.fasta | paste - - | sed -e 's/^>//' | awk -F '\t' '{print $2"\t"$1}' | sort -k2,2nr > ${outdir1}_mer_counts.normalised.tsv
+#cat mer_counts.fasta | tr "\n" "\t" | sed -e 's/\t>/\n/g' -e 's/^>//' | awk -F'\t' '{print $2"\t"$1}' >mer_counts.normalised.tsv
--- functional_impact/get_functional_impact.sh
+++ functional_impact/get_functional_impact.sh
@@ -9,7 +9,7 @@ edit_distance=$4
 threads=$5
 output_directory=$6
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 6 ]; then
   echo "Usage: bash get_functional_impact.sh reference.fa kmers.fa annotation.gff edit_distance threads " >&2 ;
   echo "Exiting now!!!"
   exit 1 ;
@@ -20,11 +20,11 @@ fi
 mrsfast --index ${reference} -e ${edit_distance}
 
 # Mapping k-mers
-mrsfast --search ${reference} --seq ${kmers_seq} --threads ${threads} -o mappings.sam
+mrsfast --search ${reference} --seq ${kmers_seq} --threads ${threads} -o ${output_directory}/mappings.sam
 
 # Calling variants
-freebayes -f ${reference} --min-alternate-count 1 mappings.sam > output.vcf
+freebayes -f ${reference} --min-alternate-count 1 mappings.sam > ${output_directory}/output.vcf
 
 # Functional impact wit VEP
 # GFF file must have index .tbi
-vep -i output.vcf -o output.vep.txt --gff ${gff_file} --fasta ${reference}
+vep -i output.vcf -o ${output_directory}/output.vep.txt --gff ${gff_file} --fasta ${reference}
